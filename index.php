1ad7ce608dfbf051a231a8c7a8a2d9b45083881d
rayansilva <rayancs4@gmail.com>
2015-12-27 18:15:25 -0200

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html> 
+<html lang=en> 
+	<head>  
+		<meta charset=utf-8>
+		<meta http-equiv=X-UA-Compatible content="IE=edge"> 
+		<meta name=viewport content="width=device-width,initial-scale=1"> 
+		<meta name=description content="Bootstrap, a sleek, intuitive, and powerful mobile first front-end framework for faster and easier web development."> 
+		<meta name=keywords content="HTML, CSS, JS, JavaScript, framework, bootstrap, front-end, frontend, web development"> 
+		<meta name=author content="Mark Otto, Jacob Thornton, and Bootstrap contributors"> 
+		<title> Cenexão </title>
+  		<!-- Bootstrap -->
+    	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+   	 	<!--[if lt IE 9]>
+      	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    	<![endif]-->
+  	</head>
+  	<body>
+    <h1>TESTE</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
+</html>
